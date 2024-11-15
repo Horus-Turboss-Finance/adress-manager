6118e5548afa729b70f42f2686d84a0bd4bde284
Docteur_Turboss <81076765+docteur-turboss@users.noreply.github.com>
2024-11-15 10:54:05 +0100

    feat: ajout de logs sur requêtes pour traçabilité des erreurs

--- src/services/adress/adress.routes.ts
+++ src/services/adress/adress.routes.ts
@@ -1,9 +1,11 @@
 import { addService, deleteService, readService, updateService } from './adressController';
 import { controleOrigine } from '../../middleware/auth';
 import express from "express";
+import { LogRequest } from '../../config/log';
 
 const router = express.Router()
 router.use(controleOrigine)
+router.use(LogRequest)
 
 router.route('/service')
 .delete(deleteService)
