5147ac6b6e48e5feb2af9e9ac17f0ee1610d4a39
Docteur_Turboss <81076765+docteur-turboss@users.noreply.github.com>
2024-11-15 10:05:36 +0100

    fix(routines): correction des bugs pour les routines de vérifications

--- src/services/routines/watchServices.ts
+++ src/services/routines/watchServices.ts
@@ -4,43 +4,52 @@ import { downAfterControle, ping, updateAfterControl } from "../../utils/request
 export const routineCheck = async () => {
   const service = await Service.find()
 
-  let pingAverage = Math.floor((30 * 60 * 950) / service.length), i = 0
-  if(service.length > 0) return setTimeout(() => {
-    routineCheck()
-  }, 60*1000);
-  // }, 30*60*1000);
+  if(service.length > 0) {
+    let pingAverage = Math.floor((60 * 950) / service.length), i = 0
+    // let pingAverage = Math.floor((30 * 60 * 950) / service.length), i = 0
   
-  let callService = async () => {
-    try{
-      await ping({adressIP : service[i].adressIP, port : service[i].port})
-    }catch(e : any){
-      if (!e.response) return 
-      // The request was made and the server responded with a status code
-      // that falls out of the range of 2xx
-      if(e.response.status == 418){
-        updateAfterControl({
-          adressIP: service[i].adressIP,
-          service : service[i].service,
-          port : service[i].port,
-          status : 2            
-        }).catch(()=>{})
-      }else{
-        downAfterControle({
-          adressIP: service[i].adressIP,
-          service : service[i].service,
-          port : service[i].port,
-        }).catch(()=>{})
+    let callService = async () => {
+      try{
+        await ping({adressIP : service[i].adressIP, port : service[i].port})
+      }catch(e : any){
+        if(e.code == "ECONNREFUSED"){
+          downAfterControle({
+            adressIP: service[i].adressIP,
+            service : service[i].service,
+            port : service[i].port,
+          }).catch(()=>{})
+        }
+        
+        if (e.response)  {
+          // The request was made and the server responded with a status code
+          // that falls out of the range of 2xx
+          if(e.response.status == 418){
+            updateAfterControl({
+              adressIP: service[i].adressIP,
+              service : service[i].service,
+              port : service[i].port,
+              status : 2            
+            }).catch(()=>{})
+          }else{
+            downAfterControle({
+              adressIP: service[i].adressIP,
+              service : service[i].service,
+              port : service[i].port,
+            }).catch(()=>{})
+          }
+        }
+      }finally{
+        i ++
       }
-    }finally{
-      i ++
+  
+      return setTimeout(() => {
+        if(i > service.length) return
+        callService()
+      }, pingAverage);
     }
-
-    return setTimeout(() => {
-      if(i > service.length) return
-      callService()
-    }, pingAverage);
+  
+    callService();
   }
-
 
   return setTimeout(() => {
     routineCheck()
